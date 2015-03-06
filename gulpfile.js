57b8bb3697a8a0f51b6684db321b04b0adc6b901
Nicholas Baroni <nick@rhythnic.com>
2015-03-06 07:29:15 -0500

    Added gulp-connect to workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,28 @@
-var gulp    = require('gulp'),
-    stylus  = require('gulp-stylus'),
-    nib     = require('nib'),
-    jeet    = require('jeet'),
-    axis    = require('axis'),
-    rupture = require('rupture'),
-    shift   = require('./');
+var gulp      = require('gulp'),
+    stylus    = require('gulp-stylus'),
+    connect = require('gulp-connect'),
+    nib       = require('nib'),
+    jeet      = require('jeet'),
+    axis      = require('axis'),
+    rupture   = require('rupture'),
+    shift     = require('./');
 
 gulp.task('styl', function () {
   gulp.src('./docs/docs.styl')
     .pipe(stylus({ use: [nib(), jeet(), axis(), rupture(), shift()], compress: true }))
-    .pipe(gulp.dest('./docs/css'));
+    .pipe(gulp.dest('./docs/css'))
+    .pipe(connect.reload());
+});
+ 
+gulp.task('connect', function() {
+  connect.server({
+    root: '',
+    livereload: true
+  });
 });
 
 gulp.task('watch', function() {
     gulp.watch('docs/*.styl', ['styl']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['connect', 'watch']);
